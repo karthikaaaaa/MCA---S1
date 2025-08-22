8afa5217277ef0644b2feb642c21d1a0dd4b0706
ajitali <ajitabi123@gmail.com>
2025-08-22 12:14:12 +0530

    Aug 22

--- C/C/L.L-Insertion.c
+++ C/C/L.L-Insertion.c
@@ -16,12 +16,73 @@ void insertionAtFront(int value)
     head=newnode; 
 }
 
+void insertionAtRear(int value)
+{
+    struct node* newnode=(struct node*) malloc(sizeof(struct node));
+    newnode->data=value;
+    newnode->next=NULL;
+    
+    if(head==NULL)
+    {
+        head=newnode;
+    }
+
+    else{
+    struct node* temp=head;      //for(temp=head;temp!=NULL;temp=temp->next)
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=newnode;}
+}
+
 void display()
 {
     struct node* temp=head;
     while(temp!=NULL)
     {
-        printf("%d",temp->data);
+        printf("%d ",temp->data);
+        temp=temp->next;
     }
-    temp=temp->next;
+}
+
+int main()
+{
+    int option;
+    while(1){
+    printf("\nDo you want to enter at the 1.front or 2.rear? ");
+    scanf("%d",&option);
+    if(option==1)
+    {
+        int i,num,val;
+        printf("How many elements do you want ot enter? ");
+        scanf("%d",&num);
+
+        printf("Enter %d elements to be inserted: ",num);
+        for(i=0;i<num;i++)
+        {
+          scanf("%d",&val);
+          insertionAtFront(val);
+        }
+    }
+
+    if(option==2)
+    {
+        int i,num,val;
+        printf("How many elements do you want ot enter? ");
+        scanf("%d",&num);
+
+        printf("Enter %d elements to be inserted: ",num);
+        for(i=0;i<num;i++)
+        {
+          scanf("%d",&val);
+          insertionAtRear(val);
+        }
+    }
+
+    printf("The elements of the list are: ");
+    display();
+    }
+
+    return 0;
 }
